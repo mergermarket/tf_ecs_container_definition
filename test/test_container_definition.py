202d7d11a4dd093e7bf252b12e75d625fcbf7460
grahamlyons <graham@grahamlyons.com>
2018-11-08 15:25:54 +0000

    Recommit the test for this

--- test/test_container_definition.py
+++ test/test_container_definition.py
@@ -108,7 +108,7 @@ class TestContainerDefinition(unittest.TestCase):
 
         # then
         assert [ { 'containerPort': 7654 } ] == definition['portMappings']
-        
+
     def test_labels(self):
         # Given
         variables = {
@@ -240,3 +240,19 @@ class TestContainerDefinition(unittest.TestCase):
             'readOnly': True
             }] == definition['mountPoints']
 
+    def test_init_process_enabled(self):
+        # given
+        variables = {
+            'name': 'test-' + str(int(time.time() * 1000)),
+            'image': '123',
+            'cpu': 1024,
+            'memory': 1024,
+            'container_port': 8001
+        }
+        varsmap = {}
+
+        # when
+        definition = self._apply_and_parse(variables, varsmap)
+
+        # then
+        assert definition['linuxParameters']['initProcessEnabled']
